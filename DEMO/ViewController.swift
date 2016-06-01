fbe17dc718c961882a211d84e1084b3cd6edb242
Aecho <aecho@aethersi.com>
2016-06-01 16:13:07 +0800

    Add more items for demo

--- DEMO/ViewController.swift
+++ DEMO/ViewController.swift
@@ -39,7 +39,46 @@ class ViewController: UIViewController {
             Item(imageName: "11.jpg", from: "12:55:33", end: "10:55:45"),
             Item(imageName: "12.jpg", from: "13:11:33", end: "11:56:33"),
             Item(imageName: "13.jpg", from: "14:55:33", end: "10:55:45"),
-            ])
+            ]),
+        Group(title: "05/03", list: [
+            Item(imageName: "1.jpg", from: "10:55:33", end: "10:55:45"),
+            Item(imageName: "2.jpg", from: "11:11:33", end: "11:56:33"),
+            Item(imageName: "3.jpg", from: "12:55:33", end: "10:55:45"),
+            ]),
+        Group(title: "05/04", list: [
+            Item(imageName: "8.jpg", from: "11:11:33", end: "11:56:33"),
+            Item(imageName: "13.jpg", from: "14:55:33", end: "10:55:45"),
+            ]),
+        Group(title: "05/05", list: [
+            Item(imageName: "1.jpg", from: "10:55:33", end: "10:55:45"),
+            Item(imageName: "2.jpg", from: "11:11:33", end: "11:56:33"),
+            Item(imageName: "3.jpg", from: "12:55:33", end: "10:55:45"),
+            ]),
+        Group(title: "05/06", list: [
+            Item(imageName: "1.jpg", from: "11:11:33", end: "11:56:33"),
+            ]),
+        Group(title: "05/07", list: [
+            Item(imageName: "4.jpg", from: "11:11:33", end: "11:56:33"),
+            ]),
+        Group(title: "05/08", list: [
+            Item(imageName: "10.jpg", from: "11:11:33", end: "11:56:33"),
+            ]),
+        Group(title: "05/09", list: [
+            Item(imageName: "1.jpg", from: "10:55:33", end: "10:55:45"),
+            Item(imageName: "2.jpg", from: "11:11:33", end: "11:56:33"),
+            Item(imageName: "3.jpg", from: "12:55:33", end: "10:55:45"),
+            Item(imageName: "4.jpg", from: "13:11:33", end: "11:56:33"),
+            Item(imageName: "5.jpg", from: "14:55:33", end: "10:55:45"),
+            Item(imageName: "6.jpg", from: "15:11:33", end: "11:56:33"),
+            Item(imageName: "7.jpg", from: "16:55:33", end: "10:55:45"),
+            Item(imageName: "8.jpg", from: "11:11:33", end: "11:56:33"),
+            Item(imageName: "9.jpg", from: "10:55:33", end: "10:55:45"),
+            Item(imageName: "10.jpg", from: "11:11:33", end: "11:56:33"),
+            Item(imageName: "11.jpg", from: "12:55:33", end: "10:55:45"),
+            Item(imageName: "12.jpg", from: "13:11:33", end: "11:56:33"),
+            Item(imageName: "13.jpg", from: "14:55:33", end: "10:55:45"),
+            ]),
+        
     ]
     
     @IBOutlet weak var collectionView: UICollectionView!
